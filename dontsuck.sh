30d895f957b26d51047f3a776d17b451d47b5d1e
Golam Moula <74001687+golam71@users.noreply.github.com>
2023-08-15 15:20:12 +0600

    Create dontsuck.sh

---
+++ dontsuck.sh
@@ -0,0 +1,22 @@
+git clone https://github.com/golam71/suckless/
+cd suckless
+cd dwm
+make
+sudo make clean install
+cd..
+cd st
+make
+sudo make clean install
+cd ..
+cd dmenu
+make
+sudo make clean install
+git clone https://git.suckless.org/surf
+cd surf
+make
+sudo make install
+cd ..
+git clone https://git.suckless.org/tabbed
+cd tabbed
+make
+sudo make install
